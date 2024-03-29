47682264a6eaff98bbc8cfec152b8f0a76a57cb5
sungyo4869 <hchengjian1@gmail.com>
2024-03-29 12:36:03 +0900

    fontを使えるようにした

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,16 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
+
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
+    );
+  }
